7f6fd22d30b6b447df6f526d045c5c73424f71fe
kirisamez <hyh5678901@gmail.com>
2017-12-13 23:04:25 +0800

    first commit.

---
+++ New_ADM/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace New_ADM.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "- NAD - 动画标签";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "欢迎各式各样的调戏.";
+
+            return View();
+        }
+    }
+}
---
+++ New_ADM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(New_ADM.Startup))]
+namespace New_ADM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
